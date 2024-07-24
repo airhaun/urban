c70bd3ed487abc047580e46d18b824418be3d9b5
airhaun <63139803+airhaun@users.noreply.github.com>
2024-07-24 18:47:16 +0300

    Update рассылка писем.py

--- рассылка писем.py
+++ рассылка писем.py
@@ -1,12 +1,6 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
-    """
-    Функция для отправки письма.
-
-    :param message: Сообщение, которое нужно отправить.
-    :param recipient: Адрес получателя.
-    :param sender: Адрес отправителя (по умолчанию "university.help@gmail.com").
-    """
-    # Проверка на корректность e-mail
+    
+  
     def is_valid_email(email):
         return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
 
@@ -14,18 +8,18 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
         return
 
-    # Проверка на отправку самому себе
+   
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
         return
 
-    # Проверка на отправителя по умолчанию
+   
     if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
-# Примеры использования функции
+
 send_email("Привет, как дела?", "student@example.com")
 send_email("Привет, как дела?", "student@example.com", sender="custom.sender@gmail.com")
 send_email("Привет, как дела?", "student@example.com", sender="university.help@gmail.com")
